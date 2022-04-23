d74f20592ae687f6c8d64d39863e4c403367b3b7
Gerard Smit <gsmit1996@gmail.com>
2022-04-23 15:03:52 +0200

    ci: add NuGet publish

---
+++ tests/Mediator.SourceGenerator.Tests/HandlerTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Zapto.Mediator.Generator;
+using VerifyXunit;
+using Xunit;
+
+namespace Mediator.SourceGenerator.Tests;
+
+[UsesVerify]
+public class HandlerTests
+{
+    [Fact]
+    public Task GenerateHandler()
+    {
+        const string source = @"
+using MediatR;
+using Zapto.Mediator;
+
+public record Request : IRequest;
+
+public class RequestHandler : IRequestHandler<Request>
+{
+    public ValueTask<Unit> Handle(IServiceProvider provider, Request request, CancellationToken cancellationToken)
+    {
+        return default;
+    }
+}";
+
+        return TestHelper.Verify<SenderGenerator>(source);
+    }
+}
